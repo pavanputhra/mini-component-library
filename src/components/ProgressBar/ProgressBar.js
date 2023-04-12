9c53d337a24be86e2ee2ba7bc5c9312c71e99e47
Pavan Kumar <pavanputhra@gmail.com>
2023-04-12 13:35:07 +0530

    Completed progress bar and select.

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,10 +3,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Wrapper role="progressbar" aria-valuenow={value} size={size} >
+    <Bar value={value}></Bar>
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  border-radius: 4px;
+  height: ${props => PROGRESS_SIZES[props.size]};
+  background-color: ${COLORS.transparentGray15};
+  padding: ${props => props.size === 'large'? '4px' : '0px'};
+`;
+
+const Bar = styled.div`
+  width: ${props => props.value}%;
+  background-color: ${COLORS.primary};
+  height: 100%;
+  border-radius: ${props => Math.round(props.value) === 100 ? '4px' : '4px 0 0 4px'};
+`;
+
+const PROGRESS_SIZES = {
+  'large': '24px',
+  'medium': '12px',
+  'small': '8px',
+}
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -4,15 +4,48 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
+import VisuallyHidden from "../VisuallyHidden";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  // const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <IconStyled id='chevron-down' />
+      <SelectStyled value={value} onChange={onChange}>
+        {children}
+      </SelectStyled>
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  display: inline-block;
+  position: relative;
+`
+
+const SelectStyled = styled.select`
+  height: 43px;
+  width: auto;
+  padding: 12px 40px 12px 16px;
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  appearance: none;
+  color: ${COLORS.gray700};
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconStyled = styled(Icon)`
+  margin-top: -1px;
+  position: absolute;
+  right: 16px;
+  bottom: 8px;
+  pointer-events: none;
+`;
 
 export default Select;
