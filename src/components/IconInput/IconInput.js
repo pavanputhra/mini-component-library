d1800783b066692def75848d7735e07885013453
Pavan Kumar <pavanputhra@gmail.com>
2023-04-12 14:06:44 +0530

    Icon input complete

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,17 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+
+
+const sizes = {
+  small: {
+    fontSize: 14,
+  },
+  large: {
+    fontSize: 18,
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +24,61 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return (
+      <Wrapper size={sizes[size].fontSize}>
+        <Input type="text" placeholder={placeholder} size={sizes[size].fontSize} width={width} />
+        <IconStyled id={icon} size={sizes[size].fontSize} />
+        <VisuallyHidden>{label}</VisuallyHidden>
+      </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  font-size: ${props => props.size + 'px'};
+  display: inline-block;
+`
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 2px solid ${COLORS.gray300};
+  width: ${props => props.width + 'px'};
+  padding: 7px 7px 7px 22px;
+  font-size: inherit;
+  color: ${COLORS.gray700};
+  font-weight: 600;
+  outline-offset: 5px;
+  
+  &::placeholder {
+    font-size: ${props => props.size + 'px'};
+    font-weight: 100;
+    color: ${COLORS.gray500};
+  }
+  
+  &:focus {
+    border-bottom: 2px solid ${COLORS.black};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+    border-bottom: 2px solid ${COLORS.black};
+  }
+`
+
+const IconStyled = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  color: ${COLORS.gray500};
+  pointer-events: none;
+  
+  ${Input}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+
 
 export default IconInput;
